521e45c2d97e95e79bddf169b30e31209e59a5e6
kilamieaz <im.sultaan05@gmail.com>
2018-10-08 08:25:28 +0700

    Controller "Search"

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,27 @@
 // Global app controller
 import Search from './models/Search';
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+
+/** Global state of the app
+ * - search object 
+ * - current recipe object
+ * - shopping list object
+ * - liked recipes
+ */
+const state = {};
+const controlSearch = async () => {
+    // get query from view
+    const query = 'pizza'
+    if (query) {
+        // new search object and add to state
+        state.search = new Search(query);
+        // prepare UI for results
+        // search for recipes
+        await state.search.getResults();
+        // render results on UI
+        console.log(state.search.result)
+    }
+}
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
