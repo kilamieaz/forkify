85f132b560718e18bc609643073aee1bea277408
kilamieaz <im.sultaan05@gmail.com>
2018-10-13 08:05:53 +0700

    model "likes"

---
+++ src/js/models/Likes.js
@@ -0,0 +1,26 @@
+export default class Likes {
+    constructor(){
+        this.likes = [];
+    }
+     
+    addLike(id, title, author, img) {
+        const like = { id, title, author, img};
+        this.likes.push(like);
+        return like;
+    }
+
+    deleteLike (id) {
+        const index = this.like.findIndex(el => el.id === id);
+        // [2,4,8] splice(1, 2) -> returns [4, 8], original array is [2]
+        // [2,4,8] slice(1, 2) -> returns 4, original array is [2, 4, 8]
+        this.like.splice(index, 1);
+    }
+
+    isLiked(id) {
+        return this.likes.findIndex(el => el.id === id) !== -1;
+    }
+
+    getNumLikes() {
+        return this.likes.length;
+    }
+}
