ad78a8dde950ddaa545a0b00320ae4486e2214ed
kilamieaz <im.sultaan05@gmail.com>
2018-10-13 13:38:01 +0700

    bug recipe

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -8,13 +8,14 @@ const formatCount = count => {
     if (count) {
         // count 2.5 --> 2 1/2
         // count 0.5 --> 1/2
-        const [int, dec] = count.toString().split('.').map(el => parseInt(el, 10));
-        if (!dec) return count;
+        const newCount = Math.round(count * 10000) / 10000;
+        const [int, dec] = newcount.toString().split('.').map(el => parseInt(el, 10));
+        if (!dec) return newcount;
         if (int === 0 ) {
-            const fr = new Fraction(count);
+            const fr = new Fraction(newcount);
             return `${fr.numerator}/${fr.denominator}`;
         } else {
-            const fr = new Fraction(count - int);
+            const fr = new Fraction(newcount - int);
             return `${int} ${fr.numerator}/${fr.denominator}`;
         }
     }
